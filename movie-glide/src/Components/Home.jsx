e5d2e71378629b86237144b20ee1d9269bcc8567
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-09-05 16:40:36 +0100

    Update Zustand, and Home page to list movies

--- movie-glide/src/Components/Home.jsx
+++ movie-glide/src/Components/Home.jsx
@@ -1,8 +1,43 @@
 import GetStartedBtn from "./GetStartedBtn";
 import UpcomingBtn from "./UpcomingBtn";
+import { useEffect } from "react";
+import useMovieStore from "../Store/useMoviesStore";
+import { fetchPopularMovies } from "../Services/api";
+import MovieList from "./MovieList";
 
 
 function Home() {
+
+    const movieList = useMovieStore((state) => state.movieList);
+    const isLoading = useMovieStore((state) => state.isLoading);
+    const error = useMovieStore((state) => state.error);
+    const addMovies = useMovieStore((state) => state.addMovies);
+    const setLoading = useMovieStore((state) => state.setLoading);
+    const setError = useMovieStore((state) => state.setError);
+
+
+    useEffect(() => {
+    const loadMovies = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const movies = await fetchPopularMovies();
+        addMovies(movies);
+      } catch (err) {
+        setError("Failed to load movies. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    
+    // Only fetch movies if our list is empty
+    if (movieList.length === 0) {
+        loadMovies();
+    }
+  }, [movieList.length, addMovies, setLoading, setError]);
+
+
+
     return (
        <div>
             <h1 className="text-secondary text-center mb-7"> 
@@ -15,10 +50,25 @@ function Home() {
                 trend series for you to watch online and priceless
             </h2>
 
-            <div className="flex justify-center gap-8">
+            <div className="flex justify-center gap-8 mb-2">
                 <GetStartedBtn />
                 <UpcomingBtn />
             </div>
+
+            {/* Loading */}
+            {isLoading && <div className="text-center text-secondary mt-10">Loading popular movies...</div>}
+
+            {/* Error */}
+            {error && <div className="text-center text-red-500 mt-10">{error}</div>}
+
+                {/* Movie List */}
+            {!isLoading && !error && (
+                <div>
+                     <h1 className="text-2xl font-bold text-primary mb-3">Popular Movies</h1>
+                    <MovieList movies={movieList} />
+                </div>
+            )}
+
        </div>
     );
 }
--- movie-glide/src/Store/useMoviesStore.js
+++ movie-glide/src/Store/useMoviesStore.js
@@ -2,10 +2,16 @@ import { create } from 'zustand';
 
 const useMovieStore = create((set) => ({
     movieList: [],
+    isLoading: false,
+    error: null,
 
+    // Add Movies
     addMovies: (newMovies) => {
         set((state) => ({movieList : [...state.movieList, ...newMovies]}));
     },
+
+    setLoading: (status) => set({ isLoading: status }),
+    setError: (message) => set({ error: message }),
 }))
 
 export default useMovieStore;
